4e5253cca5204f4ea01f5b8a459724725bc85d95
chandupurdue <chandu.pegasian@gmail.com>
2017-07-20 11:53:13 -0500

    angular 2 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropDownDirective } from './shared/dropdown.directive';
 import { ShoppingListService} from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 
 
 
@@ -29,7 +30,8 @@ import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.compone
     ShoppingListComponent,
     ShoppingEditComponent,
     DropDownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
 
   ],
   imports: [
--- src/app/app.routing.Module.ts
+++ src/app/app.routing.Module.ts
@@ -1,3 +1,4 @@
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
@@ -9,6 +10,7 @@ const appRoutes: Routes = [
 {path : '' , redirectTo : 'recipe',  pathMatch: 'full'},
 {path : 'recipes' , component : RecipeComponent, children: [
    {path: '', component : RecipeStartComponent},
+   {path: 'new', component : RecipeEditComponent},
    {path: ':id', component : RecipeDetailComponent}
 ] },
 {path : 'shopping-list' , component : ShoppingListComponent},
